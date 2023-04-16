0c5e6cdfba793526f615edaaac3501b40394ab4b
saikiran922 <kiranvemula987@gmail.com>
2023-04-16 17:47:57 +0000

    first commit

---
+++ index.js
@@ -0,0 +1,20 @@
+const element = (
+  //   Write your code here.
+  <div className="bg-container">
+    <div>
+      <h1 className="heading">Super Over League</h1>
+    </div>
+    <div>
+      <img
+        src="https://assets.ccbp.in/frontend/react-js/rcb-img.png"
+        className="teamImages"
+      />
+      <img
+        src="https://assets.ccbp.in/frontend/react-js/csk-img.png"
+        className="teamImages"
+      />
+    </div>
+  </div>
+);
+
+ReactDOM.render(element, document.getElementById("root"));
